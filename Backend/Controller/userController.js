3e2a5bddfc925371143fff35aa9be48dc12d4a30
ChetanBasargeCB <chetancb9191@gmail.com>
2026-04-29 17:09:58 +0530

    added controller for profile

--- Backend/Controller/userController.js
+++ Backend/Controller/userController.js
@@ -1,8 +1,13 @@
 import User from '../Model/userModel.js'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv/config'
+import cookieParser from 'cookie-parser'
 
 export const registerController = async(req,res)=>{
     const{name,email,phone,password}=req.body
+    
+    console.log(name,email,phone,password)
 
     try {
 
@@ -41,10 +46,45 @@ export const loginController = async(req,res)=>{
     const isPassword = await bcrypt.compare(password,user.password)
     if(!isPassword) return res.status(401).json({message:"Invalid Password"})
 
-    return res.status(201).json({message:"Login Successfull"})
+    const token = jwt.sign({id:user._id},
+        process.env.JWT_KEY,
+        {expiresIn:"15min"}
+    )
+
+    // Cookie assinging
+    res.cookie("token",token ,{
+        httpOnly:true,
+        secure:false,
+        sameSite:"strict"
+    })
+
+    return res.status(200).json({message:"Login Successfull",token})
     } catch(err){   
+        console.log("Login sever error")
         res.status(500).json({message:"Server Error"})
     }
     
 
+}
+
+
+
+export const ProfileController = async (req, res) => {
+    try {
+        // req.user was created in the middleware above
+        const userId = req.user.id; 
+
+        // Find the user by ID and exclude the password field for security
+        const user = await User.findById(userId).select("-password");
+        console.log(user)
+        if (!user) {
+            return res.status(404).json({ message: "User not found in database" });
+        }
+
+        // Send the specific user data back to the frontend
+        res.status(200).json(user);
+        
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 }
