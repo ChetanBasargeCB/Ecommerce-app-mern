e8039fd538f239c54bec4267a0dd0b07d56030d8
ChetanBasargeCB <chetancb9191@gmail.com>
2026-04-29 17:09:17 +0530

    added profile routes with middelware

--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -1,7 +1,10 @@
 import express from 'express'
-import { loginController, registerController } from '../Controller/userController.js'
+import { loginController, ProfileController, registerController } from '../Controller/userController.js'
+import AuthProfile from '../Middelware/AuthUser.js'
 
  export const userRouter = express.Router()
 
 userRouter.post("/register",registerController)
 userRouter.post("/login",loginController)
+
+userRouter.get("/profile",AuthProfile,ProfileController)
